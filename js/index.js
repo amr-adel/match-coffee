607af3fcc18e6350ee43032ae28c88c9339b3763
Amr A. Abdelmoez <amradel88@live.com>
2020-11-30 22:04:50 +0200

    Preload card icons

--- js/index.js
+++ js/index.js
@@ -5,3 +5,13 @@ import { App } from "./App.js";
 render(h(App), document.body);
 
 export { h, Component, render };
+
+// Preload card icons
+window.onload = () => {
+  const dummyImage = new Image();
+  Array(8)
+    .fill("x")
+    .forEach((n, i) => {
+      dummyImage.src = `./images/cards/card-${i + 1}.svg`;
+    });
+};
