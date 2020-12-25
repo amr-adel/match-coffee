e69b56d5a5f639e69826f69e6c1a98b443afd77c
Amr A. Abdelmoez <amradel88@live.com>
2020-12-25 21:29:42 +0200

    test Admin SDK

--- funs/admins/admins.js
+++ funs/admins/admins.js
@@ -1,13 +1,19 @@
-// const admin = require("firebase-admin");
-// const adminCredentials = process.env.ADMIN_SDK_CERT;
+const admin = require("firebase-admin");
+const adminCredentials = process.env.ADMIN_SDK_CERT;
 
-// admin.initializeApp({
-//   credential: admin.credential.cert(adminCredentials),
-// });
+admin.initializeApp({
+  credential: admin.credential.cert(JSON.parse(adminCredentials)),
+});
 
 exports.handler = async function (event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
-  };
+  return admin
+    .auth()
+    .getUser("zavUsdNfT0cvWV6Qaaa3dxUCB3o1")
+    .then((userRecord) => {
+      return {
+        statusCode: 200,
+        body: JSON.stringify(userRecord),
+      };
+    })
+    .catch((error) => `Error fetching user data:${error}`);
 };
