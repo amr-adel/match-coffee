3915690d03d3dbedebddc8d78b1a4eac7b8e2647
Amr A. Abdelmoez <amradel88@live.com>
2020-12-27 03:24:26 +0200

    test delete doc

---
+++ functions/deleteDoc.js
@@ -0,0 +1,37 @@
+const admin = require("firebase-admin");
+const adminCredentials = process.env.ADMIN_SDK_CERT;
+
+admin.initializeApp({
+  credential: admin.credential.cert(JSON.parse(adminCredentials)),
+});
+
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+};
+
+exports.handler = async function (event, context) {
+  const senderUid = event.queryStringParameters.senderUid;
+  const uid = event.queryStringParameters.uid;
+
+  return admin
+    .firestore()
+    .collection("scores")
+    .doc(uid)
+    .delete()
+    .then(() => {
+      return {
+        statusCode: 200,
+        headers,
+        body: JSON.stringify({
+          message: "Deleted successfully!",
+        }),
+      };
+    })
+    .catch((error) => {
+      return {
+        statusCode: 500,
+        headers,
+        body: JSON.stringify(error),
+      };
+    });
+};
